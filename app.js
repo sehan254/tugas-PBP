a6c5cac4a85d02567482d990d0738da11c459aff
sehan254 <160452069+sehan254@users.noreply.github.com>
2024-09-23 11:30:17 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+const nama = document.querySelector("#Name");
+nama.textContent = "SEHAN";
+
+const NomorWhatsapp = document.querySelector("#NOWA");
+NomorWhatsapp.textContent = "082133313254";
+
+const NIM = document.querySelector("#NIM");
+NIM.textContent = "20230040298";
+
+const Hoby = document.querySelector("#Hoby");
+Hoby.textContent = "GYM";
